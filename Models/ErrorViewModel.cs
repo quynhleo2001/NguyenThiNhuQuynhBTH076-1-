ccc7294601a2a0fb0fd562f8107099d2b7f0ff62
quynhleo2001 <quynhleo2001@gmail.com>
2023-02-19 10:38:08 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThiNhuQuynhBTH076_1_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
